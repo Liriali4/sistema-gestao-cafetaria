fef76e15cac375fea8780f9cb0749d24ea035c16
Liriali4 <lirialia51@gmail.com>
2026-01-24 09:42:32 +0100

    Feat: System

---
+++ src/sistemacafeteria/SistemaCafeteria.java
@@ -0,0 +1,13 @@
+package sistemacafeteria;
+
+/**
+ *
+ * @author LiriaLi4
+ */
+public class SistemaCafeteria {
+
+    public static void main(String[] args) {
+        System.out.println("***********************");
+    }
+    
+}
