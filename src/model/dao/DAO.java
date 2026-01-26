546998eba0640dde47bb95fe9b2e8c3255808e50
Liriali4 <lirialia51@gmail.com>
2026-01-26 11:50:04 +0100

    Instância dos DAOs

---
+++ src/model/dao/DAO.java
@@ -0,0 +1,31 @@
+package model.dao;
+
+import db.DB;
+import model.dao.implementation.ClienteDaoJDBC;
+import model.dao.implementation.FuncionarioDaoJDBC;
+import model.dao.implementation.ItemPedidoDaoJDBC;
+import model.dao.implementation.PedidoDaoJDBC;
+import model.dao.implementation.ProdutoDaoJDBC;
+
+public class DAO {
+
+    public static FuncionarioDao criarFuncionarioDao() {
+        return new FuncionarioDaoJDBC(DB.getConnection());
+    }
+
+    public static ClienteDao criarClienteDao() {
+        return new ClienteDaoJDBC(DB.getConnection());
+    }
+
+    public static ProdutoDao criarProdutoDao() {
+        return new ProdutoDaoJDBC(DB.getConnection());
+    }
+
+    public static PedidoDao criarPedidoDao() {
+        return new PedidoDaoJDBC(DB.getConnection());
+    }
+
+    public static ItemPedidoDao criarItemPedidoDao() {
+        return new ItemPedidoDaoJDBC(DB.getConnection());
+    }
+}
